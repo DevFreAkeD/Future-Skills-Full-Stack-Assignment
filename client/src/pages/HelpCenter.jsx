1138399478aec42b375c656fcd96456f6eb913b4
Tushar Nagar <tusharrock146@gmail.com>
2024-08-28 23:42:15 +0530

    Update HelpCenter.jsx

--- client/src/pages/HelpCenter.jsx
+++ client/src/pages/HelpCenter.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import MainComponent from "../components/MainComponent";
 
 const HelpCenter = () => {
   return (
     <>
-      <header className="fixed top-0 w-full">
+      <header className="fixed top-0 w-full z-50">
         <Header />
       </header>
       <main className="max-h-screen mt-16">
-        Main Content
+        <MainComponent />
       </main>
       <footer className="bottom-0 w-full">
         <Footer />
