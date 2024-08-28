30693c53e131af7e663c0da0201de1688fee43b2
Tushar Nagar <tusharrock146@gmail.com>
2024-08-28 16:33:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 
@@ -11,12 +12,13 @@ app.get('/', (req, res) => {
   res.send('Hello from Help Center API!');
 });
 
-const startServer = () => {
-  try {
-    app.listen(8080, () => console.log('Server started on port 8080 http://localhost:8080'));
-  } catch (error) {
-    console.error('Error starting server:', error);
-  }
-};
+const startServer = async () => {
+    try {
+      await connectDB(process.env.MONGODB_URI);
+      app.listen(8080, () => console.log('Server started on port 8080 http://localhost:8080'));
+    } catch (error) {
+      console.error('Error starting server:', error);
+    }
+};  
 
 startServer();
