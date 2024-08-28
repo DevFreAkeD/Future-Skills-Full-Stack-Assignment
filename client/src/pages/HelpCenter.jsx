f0920679dd0b0399a2aacf9a608109347d638955
Tushar Nagar <tusharrock146@gmail.com>
2024-08-28 21:59:58 +0530

    Update HelpCenter.jsx
    Added Footer component to HelpCenter page.

--- client/src/pages/HelpCenter.jsx
+++ client/src/pages/HelpCenter.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const HelpCenter = () => {
   return (
-    <header>
+    <>
+      <header className="fixed top-0 w-full">
         <Header />
-    </header>
-  )
+      </header>
+      <main className="max-h-screen mt-16">
+        Main Content
+      </main>
+      <footer className="bottom-0 w-full">
+        <Footer />
+      </footer>
+    </>
+  );
 }
 
 export default HelpCenter;
