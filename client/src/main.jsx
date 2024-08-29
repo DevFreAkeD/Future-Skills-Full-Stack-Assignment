a5bf4d131891342291342f73c3f5361ae4e7320d
Tushar Nagar <tusharrock146@gmail.com>
2024-08-29 08:12:56 +0530

    Update main.jsx
    Updated main.jsx with AppProvider
    wrapped the app with AppProvider.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AppProvider } from './AppContent.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
+);
